71cd16848ae21286b04ee25dc3390017a6f157ff
CDMBlab <63347986+CDMBlab@users.noreply.github.com>
2022-05-05 19:42:31 +0800

    Delete SimHOEPI directory

--- SimHOEPI/genotype_probabilities.m
+++
@@ -1,8 +0,0 @@
-function [p] = genotype_probabilities(mafs)
-% GENOTYPE_PROBABILITIES Compute the probabilities associated with all genotype combinations given each MAF.
-
-m = sym(mafs);
-M = 1 - m;
-s = arrayfun(@(m, M) {[M^2, 2 * M * m, m^2]}, m, M);
-p = prod(nfold(s), 2);
-end
